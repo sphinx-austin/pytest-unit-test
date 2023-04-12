f13ebce9ce14ecb39d3b60cef68dbb170b1d81aa
sphinx-austin <austinjuniour4@gmail.com>
2023-04-12 14:54:19 +0300

    parametrize tests

--- test_math_func.py
+++ test_math_func.py
@@ -39,3 +39,33 @@ def test_product_strings():
     assert result == 'Hello Hello '
     assert type(result) is str
     assert 'Hello' in result
+
+
+# Parameterizing tests
+
+# def test_add():
+#     assert math_func.add(7, 3) == 10
+
+# def test_add():
+#     result = math_func.add('Hello', ' World')
+#     assert result == 'Hello World'
+
+# def test_add():
+#     result = math_func.add(10.5, 25.5)
+#     assert result == 36
+
+# as below
+
+
+@pytest.mark.parametrize('num1, num2, result',
+                         [
+                             (7, 3, 10),
+                             ('Hello', ' World', 'Hello World'),
+                             (10.5, 25.5, 36)
+                         ])
+def test_add(num1, num2, result):
+    assert math_func.add(num1, num2) == result
+    # result = math_func.add('Hello', ' World')
+    # assert result == 'Hello World'
+    # result = math_func.add(10.5, 25.5)
+    # assert result == 36
